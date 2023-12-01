20e8a9320fd9de2e5c10d14eda2a733a17c036cf
QuiD-0 <wodnd101@naver.com>
2023-12-01 16:36:36 +0900

    nio file

--- src/main/kotlin/com/quid/io/file/write/WriteFile.kt
+++ src/main/kotlin/com/quid/io/file/write/WriteFile.kt
@@ -1,16 +1,27 @@
 package com.quid.io.file.write
 
-class WriteFile {
+import java.io.File
+import java.nio.file.Files
+
+class WriteFile(
+    private val path: String = System.getProperty("user.home"),
+    private val fileName: String = "test.txt"
+) {
+
     fun txt() {
-        val path = System.getProperty("user.home")
-        val fileName = "$path/Downloads/test.txt"
         val text = "Hello World!"
 
-        java.io.File(fileName).writeText(text)
+        File("$path/Downloads/$fileName").writeText(text)
     }
 
+    fun nio() {
+        val text = "Hello World!"
+
+        Files.write(java.nio.file.Paths.get("$path/Downloads/$fileName"), text.toByteArray())
+    }
 }
 
 fun main(){
-    WriteFile().txt()
+    WriteFile(fileName = "io.txt").txt()
+    WriteFile(fileName = "nio.txt").nio()
 }
