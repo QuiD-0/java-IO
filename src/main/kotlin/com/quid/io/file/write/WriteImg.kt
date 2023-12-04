a8c2af707ca2ac17e19db139e83aed72c6ee97ae
QuiD-0 <wodnd101@naver.com>
2023-12-04 13:31:16 +0900

    write img

---
+++ src/main/kotlin/com/quid/io/file/write/WriteImg.kt
@@ -0,0 +1,20 @@
+package com.quid.io.file.write
+
+import java.io.File
+
+class WriteImg(
+    private val path: String = System.getProperty("user.home"),
+    private val fileName: String = "image.jpg"
+) {
+
+        fun img() {
+            val image = File("src/main/resources/asset/img.jpg").readBytes()
+
+            File("$path/Downloads/$fileName").writeBytes(image)
+        }
+
+}
+
+fun main(){
+    WriteImg(fileName = "text_img.jpg").img()
+}
