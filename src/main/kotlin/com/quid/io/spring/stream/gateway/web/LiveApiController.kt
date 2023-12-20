ae078f08cd2d3649e7d9cd519b9dfe559ef2848c
QuiD-0 <wodnd101@naver.com>
2023-12-20 16:28:33 +0900

    LiveApiController 생성

--- src/main/kotlin/com/quid/io/spring/stream/gateway/web/LiveApiController.kt
+++ src/main/kotlin/com/quid/io/spring/stream/gateway/web/LiveApiController.kt
@@ -1,5 +1,7 @@
 package com.quid.io.spring.stream.gateway.web
 
+import org.springframework.core.io.FileSystemResource
+import org.springframework.core.io.Resource
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -11,6 +13,11 @@ import org.springframework.web.bind.annotation.RestController
 class LiveApiController {
 
     @GetMapping("/{user}")
-    fun getLive(@PathVariable user: String): String = "Hello $user"
-
+    fun getLive(@PathVariable user: String): Resource =
+        if (user.contains("ts")) {
+            val dir = user.split("@")[0]
+            FileSystemResource("src/main/resources/asset/hls/$dir/$user")
+        } else {
+            FileSystemResource("src/main/resources/asset/hls/$user/$user.m3u8")
+        }
 }
