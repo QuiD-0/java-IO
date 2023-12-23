ec8ad39ee046348aa81ac9cb3b99af3442e701b1
QuiD-0 <wodnd101@naver.com>
2023-12-24 00:35:23 +0900

    음악 스트리밍 준비

---
+++ src/main/kotlin/com/quid/io/spring/media/gateway/web/MusicApiController.kt
@@ -0,0 +1,9 @@
+package com.quid.io.spring.media.gateway.web
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/music")
+class MusicApiController {
+}
