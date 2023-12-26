988c838f6f8cc623b3134bea2e389c7be9c9e536
QuiD-0 <wodnd101@naver.com>
2023-12-26 17:12:02 +0900

    radio 모드 추가

--- src/main/kotlin/com/quid/io/spring/hlsliveStream/gateway/web/LiveController.kt
+++ src/main/kotlin/com/quid/io/spring/hlsliveStream/gateway/web/LiveController.kt
@@ -8,15 +8,21 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
-@RequestMapping("/live")
 class LiveController {
     private val log = LoggerFactory.getLogger(this::class.java)
 
-    @GetMapping("/{user}")
+    @GetMapping("/live/{user}")
     fun getLivePage(@PathVariable user: String, model : Model): String {
         log.info("Serving live page for user: $user")
         model.addAttribute("user", user)
         return "live"
+    }
+
+    @GetMapping("/radio/{user}")
+    fun getLiveListPage(@PathVariable user: String, model : Model): String {
+        log.info("Serving live audio page")
+        model.addAttribute("user", user)
+        return "radio"
     }
 
 }
