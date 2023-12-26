b676326f827e747adcfe4033db4a9fabd5f64aa7
QuiD-0 <wodnd101@naver.com>
2023-12-26 18:03:38 +0900

    add radio source path

--- src/main/kotlin/com/quid/io/spring/hlsliveStream/config/LiveInfoConfig.kt
+++ src/main/kotlin/com/quid/io/spring/hlsliveStream/config/LiveInfoConfig.kt
@@ -12,12 +12,16 @@ class LiveInfoConfig {
     @Value("\${live.hls-path}")
     private lateinit var path: String
 
+    @Value("\${live.radio-path}")
+    private lateinit var radioPath: String
+
     @Bean
-    fun streamInfo(): StreamInfo = StreamInfo(path)
+    fun streamInfo(): StreamInfo = StreamInfo(path, radioPath)
 
 
     class StreamInfo(
         val path: String,
+        val radioPath: String
     ){
         private val log = LoggerFactory.getLogger(this::class.java)
 
@@ -27,6 +31,9 @@ class LiveInfoConfig {
 
         fun toTsPath(user: String): String = "$path/$user"
         fun toM3u8Path(user: String): String = "$path/$user.m3u8"
+
+        fun toRadioTsPath(user: String): String = "$radioPath/$user"
+        fun toRadioM3u8Path(user: String): String = "$radioPath/$user.m3u8"
 
         private fun checkDir(streamPath: String) {
             val dir = File(streamPath)
