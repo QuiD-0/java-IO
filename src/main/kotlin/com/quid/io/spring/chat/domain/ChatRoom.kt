e2e75b480277edc140dc6f3ccc4620538716cfad
QuiD-0 <wodnd101@naver.com>
2023-12-28 16:50:37 +0900

    mongoDB 의존성 추가

--- src/main/kotlin/com/quid/io/spring/chat/domain/ChatRoom.kt
+++ src/main/kotlin/com/quid/io/spring/chat/domain/ChatRoom.kt
@@ -2,14 +2,13 @@ package com.quid.io.spring.chat.domain
 
 import org.springframework.format.annotation.DateTimeFormat
 import java.time.LocalDateTime
-import java.util.*
+import java.util.UUID
 
-class Chat(
+class ChatRoom (
     val id: UUID = UUID.randomUUID(),
-    val chatRoomId: UUID,
-    val nickname: String,
-    val message: String,
+    val name: String,
+    val count: Int = 0,
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    val timeStamp: LocalDateTime = LocalDateTime.now(),
-) {
+    val regDate: LocalDateTime = LocalDateTime.now(),
+){
 }
