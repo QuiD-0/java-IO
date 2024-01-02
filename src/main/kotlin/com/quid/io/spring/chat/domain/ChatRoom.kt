c2472095ee80d4fe214733fad00e71c1da7a3a19
QuiD-0 <wodnd101@naver.com>
2024-01-02 10:33:33 +0900

    채팅방 카운트 제거

--- src/main/kotlin/com/quid/io/spring/chat/domain/ChatRoom.kt
+++ src/main/kotlin/com/quid/io/spring/chat/domain/ChatRoom.kt
@@ -7,7 +7,6 @@ import java.util.UUID
 class ChatRoom (
     val id: UUID = UUID.randomUUID(),
     val name: String,
-    val count: Int = 0,
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     val regDate: LocalDateTime = LocalDateTime.now(),
 ){
