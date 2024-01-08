79f71cc29237cabf314ea366644b5495fe9db2c8
QuiD-0 <wodnd101@naver.com>
2024-01-08 17:14:30 +0900

    request 분리

--- src/main/kotlin/com/quid/io/spring/chat/gateway/web/ChatAPIController.kt
+++ src/main/kotlin/com/quid/io/spring/chat/gateway/web/ChatAPIController.kt
@@ -4,6 +4,7 @@ import com.quid.io.spring.chat.usecase.SendChatUseCase
 import com.quid.io.spring.chat.domain.Chat
 import org.springframework.messaging.handler.annotation.MessageMapping
 import org.springframework.web.bind.annotation.RestController
+import java.util.*
 
 @RestController
 class ChatAPIController(
@@ -11,7 +12,19 @@ class ChatAPIController(
 ) {
 
     @MessageMapping("/chat")
-    fun sendChat(chat: Chat) {
-        pubChatting(chat)
+    fun sendChat(chat: ChatRequest) {
+        pubChatting(chat.toDomain())
+    }
+
+    data class ChatRequest(
+        val chatRoomId: UUID,
+        val nickname: String,
+        val message: String,
+    ) {
+        fun toDomain() = Chat(
+            chatRoomId = chatRoomId,
+            nickname = nickname,
+            message = message
+        )
     }
 }
